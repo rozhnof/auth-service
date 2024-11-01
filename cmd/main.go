b77a7e769b8bf7436ff762323ffb756b06d2ebdb
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2024-11-01 20:44:58 +0700

    save

--- cmd/main.go
+++ cmd/main.go
@@ -79,28 +79,3 @@ func NewLogger(cfg config.LoggerConfig) (*slog.Logger, error) {
 
 	return slog.New(handler), nil
 }
-
-type VoidWriter struct{}
-
-func (w *VoidWriter) Write(p []byte) (n int, err error) {
-	return 0, nil
-}
-
-func NewVoidLogger(cfg config.LoggerConfig) (*slog.Logger, error) {
-	var level slog.Leveler
-
-	switch cfg.Level {
-	case "debug":
-		level = slog.LevelDebug
-	case "info":
-		level = slog.LevelInfo
-	case "warning":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	}
-
-	handler := slog.NewJSONHandler(new(VoidWriter), &slog.HandlerOptions{Level: level})
-
-	return slog.New(handler), nil
-}
