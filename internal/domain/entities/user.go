0e6fb408391c1912185e1a92ae383e9459afea85
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2024-12-01 21:08:38 +0700

    feat: add register token to user entity

--- internal/domain/entities/user.go
+++ internal/domain/entities/user.go
@@ -1,6 +1,7 @@
 package entities
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -8,19 +9,23 @@ import (
 )
 
 type User struct {
-	id       uuid.UUID
-	email    string
-	password vobjects.Password
+	id        uuid.UUID
+	email     string
+	password  vobjects.Password
+	confirmed bool
 
-	accessToken  *vobjects.AccessToken
-	refreshToken *vobjects.RefreshToken
+	accessToken   *vobjects.AccessToken
+	refreshToken  *vobjects.RefreshToken
+	registerToken *vobjects.RegisterToken
 }
 
 func NewUser(email string, password vobjects.Password) *User {
 	return &User{
-		id:       uuid.New(),
-		email:    email,
-		password: password,
+		id:            uuid.New(),
+		email:         email,
+		password:      password,
+		confirmed:     false,
+		registerToken: vobjects.NewRegisterToken(),
 	}
 }
 
@@ -28,13 +33,17 @@ func NewExistingUser(
 	id uuid.UUID,
 	email string,
 	password vobjects.Password,
+	confirmed bool,
 	refreshToken *vobjects.RefreshToken,
+	registerToken *vobjects.RegisterToken,
 ) *User {
 	return &User{
-		id:           id,
-		email:        email,
-		password:     password,
-		refreshToken: refreshToken,
+		id:            id,
+		email:         email,
+		password:      password,
+		confirmed:     confirmed,
+		refreshToken:  refreshToken,
+		registerToken: registerToken,
 	}
 }
 
@@ -50,15 +59,43 @@ func (u *User) Password() vobjects.Password {
 	return u.password
 }
 
+func (u *User) Confirmed() bool {
+	return u.confirmed
+}
+
+func (u *User) Confirm() error {
+	if u.RegisterToken() == nil {
+		return errors.New("register token not exists")
+	}
+
+	if !u.RegisterToken().Valid() {
+		return errors.New("register token is invalid")
+	}
+
+	u.confirmed = true
+
+	return nil
+}
+
 func (u *User) RefreshToken() *vobjects.RefreshToken {
 	return u.refreshToken
+}
+
+func (u *User) RegisterToken() *vobjects.RegisterToken {
+	return u.registerToken
 }
 
 func (u *User) AccessToken() *vobjects.AccessToken {
 	return u.accessToken
 }
 
-func (u *User) UpdateTokens(accessTokenTTL time.Duration, refreshTokenTTL time.Duration, secretKey []byte) error {
+func (u *User) UpdateRegisterToken() error {
+	u.registerToken = vobjects.NewRegisterToken()
+
+	return nil
+}
+
+func (u *User) RefreshTokens(accessTokenTTL time.Duration, refreshTokenTTL time.Duration, secretKey []byte) error {
 	payload := vobjects.AccessTokenPayload{
 		UserID: u.id,
 		Email:  u.email,
@@ -69,13 +106,8 @@ func (u *User) UpdateTokens(accessTokenTTL time.Duration, refreshTokenTTL time.D
 		return err
 	}
 
-	rt, err := vobjects.NewRefreshToken(refreshTokenTTL)
-	if err != nil {
-		return err
-	}
-
-	u.accessToken = &at
-	u.refreshToken = &rt
+	u.accessToken = at
+	u.refreshToken = vobjects.NewRefreshToken(refreshTokenTTL)
 
 	return nil
 }
